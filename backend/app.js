4216e7bda5bd1f0d55c09207ee851fe5938980bb
Kevin Brutus <kevinbrutus@pursuit.org>
2020-08-04 22:00:56 -0400

    fix issue w/ tweets

--- backend/app.js
+++ backend/app.js
@@ -11,9 +11,9 @@ app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
 
 const usersRouter = require('./routes/users');
-// const tweetsRouter = require('./routes/tweets');
+const tweetsRouter = require('./routes/tweets');
 
 app.use('/users', usersRouter);
-// app.use('/tweets', tweetsRouter);
+app.use('/tweets', tweetsRouter);
 
 app.listen(PORT, () => console.log("Server running on port ", PORT));
